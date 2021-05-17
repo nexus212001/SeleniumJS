7b4a969146ff99153a09282a2f3cd5d964d9f7af
Hezron Tan <hezront@magenic.com>
2021-05-17 17:23:39 +0800

    Added add delete employee test case

--- tests/magenic.test.js
+++ tests/magenic.test.js
@@ -1,6 +1,9 @@
 const { timeout, driver } = require('./base-test');
 const { LoginPageModel } = require('../pagemodel/loginpagemodel');
 const { HomePageModel } = require("../pagemodel/homepagemodel");
+const { EmployeePageModel } = require('../pagemodel/employeepagemodel');
+const { Employee } = require("../datamodel/employee");
+const { del } = require('selenium-webdriver/http');
 
 describe('Test suite', () => {
     
@@ -17,6 +20,29 @@ describe('Test suite', () => {
         await loginPage.clickLoginButton();
         let homePage = new HomePageModel(this.driver);
         expect(await homePage.isPageLoaded()).toBe(true);
+    });
+
+    test('AddDeleteEmployee', async () => {
+        let employeePage = new EmployeePageModel(this.driver);
+        await employeePage.openPage();
+        expect(await employeePage.isPageLoaded()).toBe(true);
+        let createNewEmployee = await employeePage.clickCreateNewLink();
+        expect(await createNewEmployee.isPageLoaded()).toBe(true);
+        
+        let expectedEmployee = new Employee(await createNewEmployee.enterFirstName(), 
+                                await createNewEmployee.enterLastName(), 
+                                await createNewEmployee.enterAddress(), 
+                                await createNewEmployee.selectRandomState(), 
+                                await createNewEmployee.enterCity(), 
+                                await createNewEmployee.selectRandomDepartment());
+        
+        await createNewEmployee.clickCreateButton();
+        expect(await employeePage.isPageLoaded()).toBe(true);
+        let actualEmployee = await employeePage.getLastRow();
+        expect(expectedEmployee).toEqual(actualEmployee);
+        let deleteEmployeePage = await employeePage.clickDeleteOnLastEntry();
+        expect(await deleteEmployeePage.isPageLoaded()).toBe(true);
+        await deleteEmployeePage.clickDeleteButton();
     });
     
     afterAll(async () => {
